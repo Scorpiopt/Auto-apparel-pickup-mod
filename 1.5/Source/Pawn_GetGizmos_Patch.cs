65cf40d31c85e9ca1e0ade275b6d4b9cf4ed6307
Taranchuk <nfhfyxer@gmail.com>
2024-04-20 12:00:01 +0300

    update

---
+++ 1.5/Source/Pawn_GetGizmos_Patch.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace AutoApparelPickup
+{
+    [HarmonyPatch(typeof(Pawn), nameof(Pawn.GetGizmos))]
+    public static class Pawn_GetGizmos_Patch
+    {
+        public static IEnumerable<Gizmo> Postfix(IEnumerable<Gizmo> gizmos, Pawn __instance)
+        {
+            foreach (var gizmo in gizmos) 
+                yield return gizmo;
+            if (__instance.IsColonistPlayerControlled)
+            {
+                yield return new Command_Action
+                {
+                    defaultLabel = "AAP.EquipArmour".Translate(),
+                    defaultDesc = "AAP.EquipArmourDesc".Translate(),
+                    icon = ContentFinder<Texture2D>.Get("UI/EquipArmour"),
+                    action = () =>
+                    {
+                        var armor = ArmorSearchUtility.PickBestArmorFor(__instance);
+                        if (armor != null)
+                        {
+                            __instance.jobs.TryTakeOrderedJob(JobMaker.MakeJob(AAP_DefOf.AAP_EquipArmour, armor));
+                        }
+                        else
+                        {
+                            Messages.Message("AAP.NoArmourFoundToEquipFor".Translate(__instance.Named("PAWN")), MessageTypeDefOf.RejectInput);
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
---
+++ 1.5/Source/SkillJob.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse.AI;
+
+namespace AutoApparelPickup
+{
+    public class SkillJob
+    {
+        public SkillJob(SkillDef skill, Job job)
+        {
+            this.skill = skill;
+            this.job = job;
+        }
+
+        public SkillDef skill;
+        public Job job;
+    }
+}
